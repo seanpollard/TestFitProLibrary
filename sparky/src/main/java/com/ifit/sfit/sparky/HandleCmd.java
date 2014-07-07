71acc4da3af0ce8e326a4805e8542bf4774c90de
jcalmonte <jc.almonte@iconfitness.com>
2014-07-07 15:49:51 -0600

    Restructured testBitfiledRdWr() Method to test only for unsupported bitfields and and read-only supported bitfields - Cleaner code, eaiser to read, mode modular
    Addrd toString() override method to HandleCmd,java. This change was made to obtain the a string value of the current bitfield being read at any point without having to refer explicitly to it by its name
    
    Modified Gradle file to make it support string values on switch statements

--- sparky/src/main/java/com/ifit/sfit/sparky/HandleCmd.java
+++ sparky/src/main/java/com/ifit/sfit/sparky/HandleCmd.java
@@ -35,6 +35,7 @@ public class HandleCmd implements OnCommandReceivedListener
     private  double mMaxSpeed = 0.0;
     private  double mMinSpeed = 0.0;
     private ModeId mResultMode;
+
     private  double mSpeed = 0.0;
     private  double mActualSpeed = 0.0;
     private  double mIncline = 0.0;
@@ -51,11 +52,13 @@ public class HandleCmd implements OnCommandReceivedListener
     private  int mIdleTimeout = 0;
     private  int mPauseTimeout = 0;
     private KeyObject mKey;
+    private String valueToString="none";
 
 
     public HandleCmd(TestApp act) {
 
         this.mAct = act;
+        this.valueToString = "No Value!";
     }
     @Override
     public void onCommandReceived(Command cmd) {
@@ -86,6 +89,7 @@ public class HandleCmd implements OnCommandReceivedListener
 
                 try {
                     mSpeed = ((SpeedConverter)commandData.get(BitFieldId.KPH).getData()).getSpeed();
+                    this.valueToString = String.valueOf(mSpeed);
                     //System.out.println("Current Speed (TestHandleInfo): " + mSpeed);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -97,6 +101,8 @@ public class HandleCmd implements OnCommandReceivedListener
 
                 try {
                     mActualSpeed = ((SpeedConverter)commandData.get(BitFieldId.ACTUAL_KPH).getData()).getSpeed();
+                    this.valueToString = String.valueOf(mActualSpeed);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -107,6 +113,8 @@ public class HandleCmd implements OnCommandReceivedListener
                 try{
                     //currently gets a null pointer assigned as of 3/3/2014
                     mMaxSpeed = ((SpeedConverter)commandData.get(BitFieldId.MAX_KPH).getData()).getSpeed();
+                    this.valueToString = String.valueOf(mMaxSpeed);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -117,6 +125,8 @@ public class HandleCmd implements OnCommandReceivedListener
                 try{
                     //currently gets a null pointer assigned as of 3/3/2014
                     mMinSpeed = ((SpeedConverter)commandData.get(BitFieldId.MIN_KPH).getData()).getSpeed();
+                    this.valueToString = String.valueOf(mMinSpeed);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -126,6 +136,8 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.GRADE)) {
                 try {
                     mIncline = ((GradeConverter)commandData.get(BitFieldId.GRADE).getData()).getIncline();
+                    this.valueToString = String.valueOf(mIncline);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -135,6 +147,8 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.ACTUAL_INCLINE)) {
                 try {
                     mActualIncline = ((GradeConverter)commandData.get(BitFieldId.ACTUAL_INCLINE).getData()).getIncline();
+                    this.valueToString = String.valueOf(mActualIncline);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -144,6 +158,8 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.MAX_GRADE)) {
                 try {
                     mMaxIncline = ((GradeConverter)commandData.get(BitFieldId.MAX_GRADE).getData()).getIncline();
+                    this.valueToString = String.valueOf(mMaxIncline);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -153,6 +169,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.MIN_GRADE)) {
                 try {
                     mMinIncline = ((GradeConverter)commandData.get(BitFieldId.MIN_GRADE).getData()).getIncline();
+                    this.valueToString = String.valueOf(mMinIncline);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -162,6 +179,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.TRANS_MAX)) {
                 try {
                     mTransMax = ((ShortConverter)commandData.get(BitFieldId.TRANS_MAX).getData()).getValue();
+                    this.valueToString = String.valueOf(mTransMax);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -172,6 +190,7 @@ public class HandleCmd implements OnCommandReceivedListener
 
                 try {
                     mResultMode = ((ModeConverter)commandData.get(BitFieldId.WORKOUT_MODE)).getMode();
+                    this.valueToString = String.valueOf(mResultMode);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -181,6 +200,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.DISTANCE)) {
                 try{
                     mDistance = ((LongConverter)commandData.get(BitFieldId.DISTANCE).getData()).getValue();
+                    this.valueToString = String.valueOf(mDistance);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
@@ -190,6 +210,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.RUNNING_TIME)) {
                 try {
                     mRunTime = ((LongConverter) commandData.get(BitFieldId.RUNNING_TIME).getData()).getValue();
+                    this.valueToString = String.valueOf(mRunTime);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -200,6 +221,7 @@ public class HandleCmd implements OnCommandReceivedListener
                 try {
                     //currently returns the bitfield id of 13 only as of 3/4/2014
                     mCalories = ((CaloriesConverter) commandData.get(BitFieldId.CALORIES).getData()).getCalories();
+                    this.valueToString = String.valueOf(mCalories);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -209,6 +231,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.WEIGHT)) {
                 try {
                     mWeight = ((WeightConverter) commandData.get(BitFieldId.WEIGHT).getData()).getWeight();
+                    this.valueToString = String.valueOf(mWeight);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -218,6 +241,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.AGE)) {
                 try {
                     mAge = ((ByteConverter) commandData.get(BitFieldId.AGE).getData()).getValue();
+                    this.valueToString = String.valueOf(mAge);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -227,6 +251,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.FAN_SPEED)) {
                 try{
                     mFanSpeed = ((ByteConverter)commandData.get(BitFieldId.FAN_SPEED).getData()).getValue();
+                    this.valueToString = String.valueOf(mFanSpeed);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -236,6 +261,7 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.IDLE_TIMEOUT)) {
                 try {
                     mIdleTimeout = ((ShortConverter) commandData.get(BitFieldId.IDLE_TIMEOUT).getData()).getValue();
+                    this.valueToString = String.valueOf(mIdleTimeout);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -244,7 +270,8 @@ public class HandleCmd implements OnCommandReceivedListener
             //Read the Pause Timeout value off of the Brainboard
             if(commandData.containsKey(BitFieldId.PAUSE_TIMEOUT)) {
                 try {
-                    mIdleTimeout = ((ShortConverter) commandData.get(BitFieldId.PAUSE_TIMEOUT).getData()).getValue();
+                    mPauseTimeout = ((ShortConverter) commandData.get(BitFieldId.PAUSE_TIMEOUT).getData()).getValue();
+                    this.valueToString = String.valueOf(mPauseTimeout);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -254,12 +281,19 @@ public class HandleCmd implements OnCommandReceivedListener
             if(commandData.containsKey(BitFieldId.KEY_OBJECT)) {
                 try {
                     mKey = ((KeyObjectConverter) commandData.get(BitFieldId.KEY_OBJECT).getData()).getKeyObject();
+                    this.valueToString = String.valueOf(mKey);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
 
         }
+    }
+
+    //returns a string version of any of the values assigned in this class
+    @Override
+    public String toString() {
+        return this.valueToString;
     }
 
     public double getSpeed(){ return this.mSpeed; }
--- sparky/src/main/java/com/ifit/sfit/sparky/TestBitfields.java
+++ sparky/src/main/java/com/ifit/sfit/sparky/TestBitfields.java
@@ -1,9 +1,13 @@
 package com.ifit.sfit.sparky;
 
+import android.widget.Switch;
+
 import com.ifit.sparky.fecp.FecpCommand;
 import com.ifit.sparky.fecp.SystemDevice;
 import com.ifit.sparky.fecp.communication.FecpController;
 import com.ifit.sparky.fecp.interpreter.bitField.BitFieldId;
+import com.ifit.sparky.fecp.interpreter.bitField.InvalidBitFieldException;
+import com.ifit.sparky.fecp.interpreter.bitField.converter.ModeId;
 import com.ifit.sparky.fecp.interpreter.command.CommandId;
 import com.ifit.sparky.fecp.interpreter.command.WriteReadDataCmd;
 import com.ifit.sparky.fecp.interpreter.device.Device;
@@ -35,6 +39,9 @@ public class TestBitfields {
     private HandleCmd hCmd;
     private SFitSysCntrl mSFitSysCntrl;
     private SystemDevice MainDevice;
+    private String bitfieldRdWrResults = "\n\n----------------------------BITFIELDS TEST RESULTS----------------------------\n\n"; //to store results of test
+    private FecpCommand cmd;
+
 
     public TestBitfields(FecpController fecpController, TestApp act, SFitSysCntrl ctrl) {
         //Get controller sent from the main activity (TestApp)
@@ -45,6 +52,8 @@ public class TestBitfields {
             hCmd = new HandleCmd(this.mAct);// Init handlers
             //Get current system device
             MainDevice = this.mFecpController.getSysDev();
+            this.cmd = new FecpCommand(MainDevice.getCommand(CommandId.WRITE_READ_DATA),hCmd);
+
         }
         catch (Exception ex) {
             ex.printStackTrace();
@@ -52,25 +61,23 @@ public class TestBitfields {
     }
 
      //---------------------------------------------------------------------//
-     //    Test read/write operation on each bitfield- testBitfieldRdWr()   //
+     //  Test read/write access on supported read-only - testBitfieldRdWr() //
      //                                                                     //
-     //         - Send valid values to write to all the supported bitfields //
-     //         - Read values written to verify them                        //
-     //         - If read only, verify writing operation to bitfield fails  //
+     //         -  verify writing operation to bitfield fails for read only //
      //         - If bitfield is not supported, verify exception is thrown  //
      //---------------------------------------------------------------------//
 
      public String testBitfieldRdWr() throws Exception {
 
-        FecpCommand cmd = new FecpCommand(MainDevice.getCommand(CommandId.WRITE_READ_DATA),hCmd,100,1000);
+        double valueToWrite = 10;
 
-        String bitfieldRdWrResults = "\n\n----------------------------BITFIELDS TEST RESULTS----------------------------\n\n"; //to store results of test
         bitfieldRdWrResults+= "------Testing Unsupported Bitfields------\n\n"; //to store results of test
 
          ArrayList<BitFieldId> supportedBitFields = new ArrayList<BitFieldId>(MainDevice.getInfo().getSupportedBitfields());
-        //ArrayList<BitFieldId> unsupportedBitFields = new ArrayList<BitFieldId>(); //To hold unsupported BitFields
+         ArrayList<BitFieldId> supportedRdBitFields = new ArrayList<BitFieldId>(MainDevice.getInfo().getSupportedReadOnlyBitfields());
 
         //loop through all bitfields, try to read unsupported ones, verify exception is thrown
+
         for (BitFieldId bf: BitFieldId.values())
         {
             if(!supportedBitFields.contains(bf))
@@ -78,59 +85,49 @@ public class TestBitfields {
               //  unsupportedBitFields.add(bf);
               //Try to read a value from this bitfeld and verify that it throws exception
                 try{
+                    bitfieldRdWrResults += "current bitfield: "+ bf.name()+"\n";
+
                     ((WriteReadDataCmd)cmd.getCommand()).addReadBitField(bf);
                     mSFitSysCntrl.getFitProCntrl().addCmd(cmd);
                     Thread.sleep(1000);
+                    bitfieldRdWrResults += "Status trying to read unsupported bitfield: "+ bf.name() +" " + (cmd.getCommand()).getStatus().getStsId().getDescription() + "\n";
+
                     bitfieldRdWrResults+="* FAIL * NO Exception thrown when trying to read unsupported bitfield:  "+bf.name()+ "\n";
 
                 }
                 catch (Exception ex)
                 {
                     ex.printStackTrace();
+                    bitfieldRdWrResults += "Status trying to read unsupported bitfield: "+ bf.name() +" "+ (cmd.getCommand()).getStatus().getStsId().getDescription() + "\n";
                     bitfieldRdWrResults+="* PASS * Exception thrown when trying to read unsupported bitfield:  "+bf.name()+ "\n";
-                    bitfieldRdWrResults+="Details: " + ex.getMessage() +"\n\n";
+                    bitfieldRdWrResults+="Details: " + ex.toString() +"\n\n";
                 }
-
             }
         }
 
-         bitfieldRdWrResults+= "------Testing Read/Write Access for Supported Bitfields------\n\n"; //to store results of test
+         bitfieldRdWrResults+= "------Testing Read/Write Access for Supported READ-ONLY Bitfields------\n\n"; //to store results of test
 
-            //Loop through all supported fields
-        for(BitFieldId bf: supportedBitFields)
+            //Loop through all readonly supported fields
+        for(BitFieldId b: supportedRdBitFields)
         {
             //if it's readonly, try to write to it and verify exception is thrown
-            if(bf.getReadOnly())
-            {
+
                 try {
-                    ((WriteReadDataCmd) cmd.getCommand()).addWriteData(bf, 10);
+                    ((WriteReadDataCmd) cmd.getCommand()).addWriteData(b, 10);
                     mSFitSysCntrl.getFitProCntrl().addCmd(cmd);
                     Thread.sleep(1000);
-                    bitfieldRdWrResults += "* FAIL * NO Exception thrown when trying to write read-only bitfield:  " + bf.name() + "\n";
+                    bitfieldRdWrResults += "* FAIL * NO Exception thrown when trying to write read-only bitfield:  " + b.name() + "\n";
                 }
                 catch (Exception ex)
                 {
                     ex.printStackTrace();
-                    bitfieldRdWrResults+="* PASS * Exception thrown when trying to write read-only bitfield:  "+bf.name()+ "\n";
+                    bitfieldRdWrResults+="* PASS * Exception thrown when trying to write read-only bitfield:  "+b.name()+ "\n";
                     bitfieldRdWrResults+=" Details: " + ex.getMessage() +"\n\n";
                 }
             }
-        }
 
          return bitfieldRdWrResults;
     }
 
-     //-------------------------------------------------------------------------------//
-     //   Test valid input values for each bitfield - testBitfieldValuesValidation()  //
-     //                                                                               //
-     //      - Send invalid values to each bitfield and verify exception is thrown    //
-     //-------------------------------------------------------------------------------//
-
-     public String testBitfieldValuesValidation()
-     {
-         String bitfiedValuesVAlidatonResults = "";
-
-         return bitfiedValuesVAlidatonResults;
-     }
 
 }
